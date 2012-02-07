8bb8fd4d2f6a3e3f9e1b4cc1a91d5e29cfa2249e
Masato OSHIMA <qqw75pn9@gmail.com>
2012-02-08 00:55:04 +0900

    Initial Commit

---
+++ IMCSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IMCSample
+//
+//  Created by 大島 雅人 on 12/02/08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
